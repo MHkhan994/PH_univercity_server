c543dfdfe24634bd815b54c0f18abe57ff22b0e4
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-08-17 22:28:29 +0600

    delete admin

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
 import { AdminControllers } from './admin.controller'
+import validateRequest from '../../middleware/validateRequest'
+import { AdminValidations } from './admin.validation'
 
 const router = Router()
 
 router.get('/single/:id', AdminControllers.getSingleAdmin)
 
-router.get('/get-all', AdminControllers.getAllAdmins)
+router.get(
+  '/get-all',
+  validateRequest(AdminValidations.createAdminValidation),
+  AdminControllers.getAllAdmins,
+)
+
+router.delete('/delete/:id', AdminControllers.deleteAdmin)
 
 export const AdminRoutes = router
--- src/app/modules/admin/admin.services.ts
+++ src/app/modules/admin/admin.services.ts
@@ -1,6 +1,10 @@
+import httpStatus from 'http-status'
 import QueryBuilder from '../../builder/QueryBuilder'
+import AppError from '../../errors/AppError'
 import { searchableFields } from '../students/students.constants'
 import Admin from './admin.model'
+import mongoose from 'mongoose'
+import { User } from '../user/user.model'
 
 const getSingleAdminFromDB = async (id: string) => {
   const result = await Admin.findById(id)
@@ -21,7 +25,53 @@ const getAllAdminFromDB = async (query: Record<string, unknown>) => {
   return result
 }
 
+const deleteAdminFromDB = async (id: string) => {
+  const admin = await Admin.findById(id)
+
+  if (!admin) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Admin does not exist')
+  }
+
+  const session = await mongoose.startSession()
+
+  try {
+    session.startTransaction()
+
+    const deletedAdmin = await Admin.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, session },
+    )
+
+    if (!deletedAdmin) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete admin')
+    }
+
+    const deletedUser = await User.findByIdAndUpdate(
+      admin.user,
+      {
+        isDeleted: true,
+      },
+      { new: true, session },
+    )
+
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user')
+    }
+
+    await session.commitTransaction()
+    await session.endSession()
+
+    return deletedAdmin
+  } catch (err) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete admin')
+  }
+}
+
 export const AdminServices = {
   getSingleAdminFromDB,
   getAllAdminFromDB,
+  deleteAdminFromDB,
 }
